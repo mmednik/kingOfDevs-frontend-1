67a706e7198868cb836aebfab1ac3e7d0b1827af
Martín Alejandro Mednik <mmednik@gmail.com>
2023-02-09 17:41:11 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <div className={`App ${theme}-theme`}>
+      <h1>Current theme: {theme}</h1>
+      <p>
+        Light themes have lighter backgrounds and darker font colors. Meanwhile,
+        dark themes have darker backgrounds and lighter font colors. Dark themes
+        prevent <strong>Eyes Strain </strong>
+        and reduce device energy consumption
+      </p>
+      <h3>What we expect?</h3>
+      <p>The button below should toggle between light and dark mode</p>
+      <button onClick={toggleTheme}>Toggle theme</button>
+    </div>
+  );
+}
